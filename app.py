0e6f98939fa4e4a2e945b66778194fd00c5fbcf0
pawscanthack <scottthomas.davis@bellevuecollege.edu>
2023-01-31 02:19:44 -0500

    new file:   __pycache__/app.cpython-310.pyc 	new file:   app.py 	new file:   data.csv 	new file:   templates/base.html 	new file:   templates/data.html 	new file:   templates/footer.html 	new file:   templates/index.html 	new file:   templates/that.html 	new file:   templates/this.html #

---
+++ app.py
@@ -0,0 +1,28 @@
+## app.py
+
+import csv
+from flask import Flask, render_template, jsonify
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    current_year = datetime.now().year
+    return render_template('index.html', current_year=current_year)
+
+@app.route('/this')
+def this_page():
+    return render_template('this.html')
+
+@app.route('/that')
+def that_page():
+    return render_template('that.html')
+
+@app.route('/data')
+def show_data():
+    with open('data.csv', 'r') as f:
+        reader = csv.reader(f)
+        headers = next(reader)
+        data = list(reader)
+    return render_template('data.html', headers=headers, data=data)
